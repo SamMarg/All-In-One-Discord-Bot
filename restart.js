50c78fd6a721ba0aadff37f01a5d1535a0baa377
Sam <114945325+SamMarg@users.noreply.github.com>
2025-04-12 16:46:08 +0200

    Update restart.js
    This is currently being fixed, as it is not working like I wanted it to.

--- restart.js
+++ restart.js
@@ -1,7 +1,6 @@
 const { exec } = require('child_process');
 
 setTimeout(() => {
-    // Run main.js with the -r flag
     exec('node main.js -r', (err, stdout, stderr) => {
         if (err) {
             console.error(`Error restarting main.js: ${err}`);
@@ -14,8 +13,7 @@ setTimeout(() => {
         console.log(`stdout: ${stdout}`);
     });
 
-    // Wait another 2 seconds before exiting
     setTimeout(() => {
         process.exit();
-    }, 2000);  // Wait for 2 seconds before exiting
+    }, 2000);
 }, 2000);
